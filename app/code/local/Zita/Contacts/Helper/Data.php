f6fbd21d0afe5bfeb1e66e0553978e8de79abf0e
Zita <wildcatzita@gmail.com>
2015-12-08 16:17:31 +0200

    Rewrite for Mage_Contacts_Helper_Data getUserName() in the module

---
+++ app/code/local/Zita/Contacts/Helper/Data.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Zita
+ * Date: 08.12.2015
+ * Time: 12:15
+ */
+
+class Zita_Contacts_Helper_Data extends Mage_Contacts_Helper_Data
+{
+    public function getUserName()
+    {
+        if (!Mage::getSingleton('customer/session')->isLoggedIn()) {
+            return 'Joe';
+        }
+        $customer = Mage::getSingleton('customer/session')->getCustomer();
+        return trim($customer->getName());
+    }
+}
