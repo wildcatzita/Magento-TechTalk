1a4ad43280017f786c4c673a42d949332eb43bfc
Zita <wildcatzita@gmail.com>
2015-12-14 11:40:05 +0200

    Create frontend controller for  Ainstainer_Contacts with form

---
+++ app/code/local/Ainstainer/Contacts/controllers/IndexController.php
@@ -0,0 +1,12 @@
+<?php
+
+
+
+class Ainstainer_Contacts_IndexController extends Mage_Core_Controller_Front_Action {
+
+    public function indexAction()
+    {
+        $this->loadLayout();
+        $this->renderLayout();
+    }
+}
